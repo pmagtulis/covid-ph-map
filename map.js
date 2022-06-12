b5efc483a2eddfa1a13fc663157fca5e0a31181d
Prinz Magtulis <87161563+pmagtulis@users.noreply.github.com>
2022-06-11 20:35:46 -0400

    Add files via upload

--- map.js
+++ map.js
@@ -4,18 +4,17 @@ var map = new mapboxgl.Map({
     style: 'mapbox://styles/pmagtulis07/cl47r8lhp001014n747gw9ltk',
     zoom: 3,
     maxZoom: 9,
-    minZoom: 3,
-    center: [11.404, 121.630],
-    projection: 'albers',
+    minZoom: 5,
+    center: [121.630, 11.404]
 });
 
 map.on("load", function () {
     map.addLayer({
-      id: "us_states_elections_outline",
+      id: "regions_outline",
       type: "line",
       source: {
         type: "geojson",
-        data: "data/statesElections.geojson",
+        data: "data/simplified_regions.geojson",
       },
       paint: {
         "line-color": "#ffffff",
@@ -23,41 +22,29 @@ map.on("load", function () {
       },
     });
     map.addLayer({
-      id: "us_states_elections",
+      id: "regions_cases",
       type: "fill",
       source: {
         type: "geojson",
-        data: "data/statesElections.geojson",
+        data: "data/simplified_regions.geojson",
       },
       maxzoom: 6, 
-      paint: {
-        "fill-color": [
-          "match",
-          ["get", "Winner"],
-          "Donald J Trump", "#cf635d",
-          "Joseph R Biden Jr", "#6193c7",
-          "Other", "#91b66e",
-          "#ffffff",
-        ],
-        "fill-outline-color": "#ffffff",
-        "fill-opacity": [
-            "step",
-            ["get", "WnrPerc"],
-            0.3, 0.4,
-            0.5, 0.5,
-            0.7, 0.6,
-            0.9
-        ]
-      },
+      "paint": {
+        "fill-color": {
+            property: "covid_cases",
+            stops: [[0, 'grey'],
+                    [1100000, 'red']]
+        }
+    }
     }, "waterway-label");
 
     map.addLayer(
         {
-          id: "us_counties_elections_outline",
+          id: "cities_outline",
           type: "line",
           source: {
             type: "geojson",
-            data: "data/countiesElections.geojson",
+            data: "data/simplified_provinces.geojson",
           },
           minzoom: 6,
           paint: {
@@ -65,93 +52,65 @@ map.on("load", function () {
             "line-width": 0.25,
           },
         },
-        "us_states_elections"
+        "regions_cases"
       );
       map.addLayer(
         {
-          id: "us_counties_elections",
+          id: "cities_cases",
           type: "fill",
           source: {
             type: "geojson",
-            data: "data/countiesElections.geojson",
+            data: "data/simplified_provinces.geojson",
           },
           minzoom: 6,
-          paint: {
-            "fill-color": [
-              "match",
-              ["get", "Winner"],
-              "Donald J Trump",
-              "#cf635d",
-              "Joseph R Biden Jr",
-              "#6193c7",
-              "Other",
-              "#91b66e",
-              "#ffffff",
-            ],
-            "fill-outline-color": "#000000",
-            "fill-opacity": [
-              "step",
-              ["get", "WnrPerc"],
-              0.3,
-              0.4,
-              0.5,
-              0.5,
-              0.7,
-              0.6,
-              0.9,
-            ],
-          },
+          "paint": {
+            "fill-color": {
+                property: "covid_cases",
+                stops: [[0, 'grey'],
+                        [300000, 'red']]
+            }
+        }
         },
-        "us_counties_elections_outline"
+        "cities_outline"
       );
   });
 
 
 // Create the popup
-map.on('click', 'us_states_elections', function (e) {
-    var stateName = e.features[0].properties.State;
-    var winner = e.features[0].properties.Winner;
-    var wnrPerc = e.features[0].properties.WnrPerc;
-    var totalVotes = e.features[0].properties.Total;
-    wnrPerc = (wnrPerc * 100).toFixed(0);
-    totalVotes = totalVotes.toLocaleString();
-    stateName = stateName.toUpperCase();
+map.on('click', 'regions_cases', function (e) {
+    var regionName = e.features[0].properties.regions;
+    var covidCases = e.features[0].properties.covid_cases;
+    regionName = regionName.toUpperCase();
     new mapboxgl.Popup()
         .setLngLat(e.lngLat)
-        .setHTML('<h4>'+stateName+'</h4>'
-            +'<h2>'+winner+'</h2>'
-            + '<p>'+wnrPerc+'% - ('+totalVotes+' votes)</p>')
+        .setHTML('<h4>'+regionName+'</h4>'
+            +'<h2>'+covidCases+'</h2>')
         .addTo(map);
 });
 // Change the cursor to a pointer when the mouse is over the us_states_elections layer.
-map.on('mouseenter', 'us_states_elections', function () {
+map.on('mouseenter', 'regions_cases', function () {
     map.getCanvas().style.cursor = 'pointer';
 });
 // Change it back to a pointer when it leaves.
-map.on('mouseleave', 'us_states_elections', function () {
+map.on('mouseleave', 'regions_cases', function () {
     map.getCanvas().style.cursor = '';
 });
 
-map.on('click', 'us_counties_elections', function (e) {
-    var stateName = e.features[0].properties.State;
-    var countyName = e.features[0].properties.County;
-    var winner = e.features[0].properties.Winner;
-    var wnrPerc = e.features[0].properties.WnrPerc;
-    var totalVotes = e.features[0].properties.Total;
-    wnrPerc = (wnrPerc * 100).toFixed(0);
-    totalVotes = totalVotes.toLocaleString();
-    stateName = stateName.toUpperCase();
-    countyName = countyName.toUpperCase();
+map.on('click', 'cities_cases', function (e) {
+    var regionName = e.features[0].properties.adm1_en;
+    var provinceName = e.features[0].properties.adm2_en;
+    var covidCases = e.features[0].properties.covid_cases;
+    regionName = regionName.toUpperCase();
+    provinceName = provinceName.toUpperCase();
     new mapboxgl.Popup()
         .setLngLat(e.lngLat)
-        .setHTML('<h4>' + countyName + ' - ' + stateName + '</h4>'
-            + '<h2>' + winner + '</h2>'
-            + '<p>' + wnrPerc + '% - (' + totalVotes + ' votes)</p>')
+        .setHTML('<h4>' + provinceName + ' - ' + regionName + '</h4>'
+            + '<h2>' + covidCases + '</h2>')
         .addTo(map);
 });
-map.on('mouseenter', 'us_counties_elections', function () {
+map.on('mouseenter', 'cities_cases', function () {
     map.getCanvas().style.cursor = 'pointer';
 });
-map.on('mouseleave', 'us_counties_elections', function () {
+map.on('mouseleave', 'cities_cases', function () {
     map.getCanvas().style.cursor = '';
 });
