ac83f856b4e74e451f8f40c621eb4dd07fb41d70
Prinz Magtulis <87161563+pmagtulis@users.noreply.github.com>
2022-06-28 15:03:06 -0400

    Add files via upload

--- map.js
+++ map.js
@@ -1,27 +1,22 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicG1hZ3R1bGlzMDciLCJhIjoiY2wzdTgyNzh0MjlqNjNjbTl4YWdyczE2aiJ9.OusPbpMc7Ue0YyVgHINiAA';
-const bounds = [
-  [-106.52384311422753, 26.336565113655656], // Southwest coordinates
-  [-92.93354123119515, 36.694146838114285] // Northeast coordinates
-  ];
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/pmagtulis07/cl4bop9c5000d14qweqz24ghl',
-    zoom: 5,
-    maxZoom: 8,
-    minZoom: 5,
-    center: [-97.854, 32.103],
-    projection:'albers',
-    maxBounds: bounds
+    style: 'mapbox://styles/pmagtulis07/cl47r8lhp001014n747gw9ltk',
+    zoom: 4,
+    maxZoom: 9,
+    minZoom: 3,
+    center: [121.630, 11.404],
+    // maxBounds: [[104.7, 10.647], [140.59, 10.6]]
 });
 
 map.on("load", function () {
   map.resize();
     map.addLayer({
-      id: "texas_outline",
+      id: "regions_outline",
       type: "line",
       source: {
         type: "geojson",
-        data: "https://raw.githubusercontent.com/pmagtulis/mapping/main/wildfires/data/countiesWildfire.geojson",
+        data: "data/simplified_regions.geojson",
       },
       paint: {
         "line-color": "#ffffff",
@@ -29,54 +24,189 @@ map.on("load", function () {
       },
     });
     map.addLayer({
-      id: "wildfire_numbers",
+      id: "regions_cases",
       type: "fill",
       source: {
         type: "geojson",
-        data: "https://raw.githubusercontent.com/pmagtulis/mapping/main/wildfires/data/countiesWildfire.geojson",
+        data: "data/simplified_regions.geojson",
       },
-      maxzoom: 8, 
-      minzoom: 5,
+      maxzoom: 5, 
+      minzoom: 4,
       "paint": {
         "fill-color": [
           "match",
           ["get", "percentiles"],
-          "0-10", "#5e4fa2",
-          "11-20", "#3288bd",
-          "21-30", "#66c2a5",
-          "31-40", "#abdda4",
-          "41-50", "#e6f598",
-          "51-60", "#fee08b",
-          "61-70", "#fdae61",
-          "71-80", "#f46d43",
-          "81-90", "#d53e4f",
-          "91-100", "#9e0142",
+          "0-1000", "#f2e926",
+          "1001-2000", "#efd124",
+          "2001-3000", "#ebb822",
+          "3001-4000", "#e8a01f",
+          "4001-5000", "#e5871d",
+          "5001-6000", "#e16f1b",
+          "6001-7000", "#de5619",
+          "7001-8000", "#db3e16",
+          "8001-9000", "#ea3322",
+          "9001-10000", "#d72514",
           "#ffffff",
         ],
     }
     }, "waterway-label");
+
+    map.addLayer(
+        {
+          id: "cities_outline",
+          type: "line",
+          source: {
+            type: "geojson",
+            data: "data/simplified_provinces.geojson",
+          },
+          minzoom: 5,
+          maxzoom: 7,
+          paint: {
+            "line-color": "#ffffff",
+            "line-width": 0.25,
+          },
+        },
+        "regions_cases"
+      );
+      map.addLayer(
+        {
+          id: "cities_cases",
+          type: "fill",
+          source: {
+            type: "geojson",
+            data: "data/simplified_provinces.geojson",
+          },
+          minzoom: 5,
+          maxzoom: 7,
+          "paint": {
+            "fill-color": [
+              "match",
+              ["get", "percentiles"],
+              "0-1000", "#f2e926",
+              "1001-2000", "#efd124",
+              "2001-3000", "#ebb822",
+              "3001-4000", "#e8a01f",
+              "4001-5000", "#e5871d",
+              "5001-6000", "#e16f1b",
+              "6001-7000", "#de5619",
+              "7001-8000", "#db3e16",
+              "8001-9000", "#ea3322",
+              "9001-10500", "#d72514",
+              "#ffffff",
+            ],
+        }
+        },
+        "cities_outline"
+      );
+
+
+      map.addLayer(
+        {
+          id: "municipal_outline",
+          type: "line",
+          source: {
+            type: "geojson",
+            data: "data/simplified_municipalities.geojson",
+          },
+          minzoom: 7,
+          paint: {
+            "line-color": "#ffffff",
+            "line-width": 0.25,
+          },
+        },
+        "cities_cases"
+      );
+      map.addLayer(
+        {
+          id: "municipal_cases",
+          type: "fill",
+          source: {
+            type: "geojson",
+            data: "data/simplified_municipalities.geojson",
+          },
+          minzoom: 7,
+          "paint": {
+          "fill-color": [
+            "match",
+            ["get", "percentiles"],
+            "0-200", "#f2e926",
+            "201-400", "#efd124",
+            "401-600", "#ebb822",
+            "601-800", "#e8a01f",
+            "801-1000", "#e5871d",
+            "1001-1200", "#e16f1b",
+            "1201-1400", "#de5619",
+            "1401-1600", "#db3e16",
+            "1601-1800", "#ea3322",
+            "1801-2000", "#d72514",
+            "#ffffff",
+          ],
+        },
+      }, "municipal_outline"
+      );
   });
+
+  
 
 
 // Create the popup
-map.on('click', 'wildfire_numbers', function (e) {
-    var countyName = e.features[0].properties.name_y;
-    var burnedPct = e.features[0].properties.pct_burned.toLocaleString('en-US');
-    var housingUnits = e.features[0].properties.housing_units.toLocaleString('en-US');
-    var burnedHousing = e.features[0].properties.exposed_units.toLocaleString('en-US');
+map.on('click', 'regions_cases', function (e) {
+    var regionName = e.features[0].properties.regions;
+    var casePerPop = e.features[0].properties.case_per_pop.toLocaleString('en-US');
+    var covidCases = e.features[0].properties.covid_cases.toLocaleString('en-US');
+    regionName = regionName.toUpperCase();
     new mapboxgl.Popup()
         .setLngLat(e.lngLat)
-        .setHTML('<h1>'+ '<strong>' + countyName + '</strong>' + '</h1>'
-            + '<h2>' + 'Total housing units:' + " " + housingUnits + '</h2>'
-            + '<h2>' + 'Total units at risk from wildfires:' + " " + '<strong>' + burnedHousing + '</strong>' + '</h2>'
-            +'<h1>' + '% of total houses' + " " + '<strong>' + burnedPct + '%' + '</strong>' + '</h1>')
+        .setHTML('<h2>'+ '<strong>' + regionName + '</strong>' + '</h2>'
+            + '<h2>' + 'Number of cases:' + " " + covidCases + '</h2>'
+            +'<h1>' + 'Case per 100,000:' + " " + '<strong>' + casePerPop + '</strong>' + '</h1>')
         .addTo(map);
 });
-// Change the cursor to a pointer when the mouse is over the map
-map.on('mouseenter', 'wildfire_numbers', function () {
+// Change the cursor to a pointer when the mouse is over the us_states_elections layer.
+map.on('mouseenter', 'regions_cases', function () {
     map.getCanvas().style.cursor = 'pointer';
 });
 // Change it back to a pointer when it leaves.
-map.on('mouseleave', 'wildfire_numbers', function () {
+map.on('mouseleave', 'regions_cases', function () {
     map.getCanvas().style.cursor = '';
+});
+
+map.on('click', 'cities_cases', function (e) {
+    var regionName = e.features[0].properties.adm1_en;
+    var casePerPop = e.features[0].properties.case_per_pop.toLocaleString('en-US');
+    var provinceName = e.features[0].properties.province;
+    var covidCases = e.features[0].properties.covid_cases.toLocaleString('en-US');
+    regionName = regionName.toUpperCase();
+    new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML('<h2>'+ '<strong>' + provinceName + '</strong>' + '-' + regionName + '</h2>'
+            + '<h2>' + 'Number of cases:' + " " + covidCases + '</h2>'
+            +'<h1>' + 'Case per 100,000:' + " " + '<strong>' + casePerPop + '</strong>' + '</h1>')
+        .addTo(map);
+});
+map.on('mouseenter', 'cities_cases', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'cities_cases', function () {
+    map.getCanvas().style.cursor = '';
+});
+
+map.on('click', 'municipal_cases', function (e) {
+  var provinceName = e.features[0].properties.province;
+  var municipalName = e.features[0].properties.municipality;
+  var casePerPop = e.features[0].properties.case_per_pop.toLocaleString('en-US');
+  var covidCases = e.features[0].properties.covid_cases.toLocaleString('en-US');
+  provinceName = provinceName.toUpperCase();
+  new mapboxgl.Popup()
+      .setLngLat(e.lngLat)
+      .setHTML('<h2>'+ '<strong>' + municipalName + '</strong>' + '-' + provinceName + '</h2>'
+            + '<h2>' + 'Number of cases:' + " " + covidCases + '</h2>'
+            +'<h1>' + 'Case per 10,000:' + " " + '<strong>' + casePerPop + '</strong>' + '</h1>')
+      .addTo(map);
+});
+map.on('mouseenter', 'municipal_cases', function () {
+  map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'municipal_cases', function () {
+  map.getCanvas().style.cursor = '';
 });
